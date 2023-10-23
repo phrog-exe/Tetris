e5c571be81f00f00cd79c3121442726b34f27434
Weronika <91962861+s24404@users.noreply.github.com>
2023-10-24 00:02:15 +0200

    Add project files.

---
+++ Block.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffsett { get; }
+
+        public abstract int Id { get; }
+
+        private int rotationState;
+        private Position offsett;
+
+        public Block()
+        {
+            offsett = new Position(StartOffsett.Row, StartOffsett.Column); 
+        }
+
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach(Position p in Tiles[rotationState]) 
+            {
+                yield return new Position(p.Row + offsett.Row, p.Column + offsett.Column);
+            }
+        }
+        //rotacja według zegara
+        public void RotateCW()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+        //rotacja przeciwna do zegara
+        public void RotateCCW()
+        {
+            if(rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
+
+        }
+        public void Move(int rows, int columns)
+        {
+            offsett.Row += rows;
+            offsett.Column += columns;
+        }
+        public void Reset()
+        {
+            rotationState = 0;
+            offsett.Row = StartOffsett.Row;
+            offsett.Column = StartOffsett.Column;
+                
+        }
+    }
+
+}
---
+++ Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class Position
+    {
+        //gettery i settery
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        //konstruktor (SKRÓTY CTRL + .)
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+    }
+}
