ebf7c4f1c2db36a4bf87425faf482f1baa83ed33
Weronika <91962861+s24404@users.noreply.github.com>
2023-10-30 15:02:25 +0100

    Final game of tetris

--- BlockQueue.cs
+++ BlockQueue.cs
@@ -10,8 +10,8 @@ namespace Tetris
     
         public class BlockQueue
         {
-            private readonly Block[] blocks = new Block[]
-            {
+        private readonly Block[] blocks = new Block[]
+    {
             new IBlock(),
             new JBlock(),
             new LBlock(),
@@ -19,34 +19,35 @@ namespace Tetris
             new SBlock(),
             new TBlock(),
             new ZBlock()
-            };
+    };
 
-            private readonly Random random = new Random();
+        private readonly Random random = new Random();
 
-            public Block NextBlock { get; private set; }
+        public Block NextBlock { get; private set; }
 
-            public BlockQueue()
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
             {
                 NextBlock = RandomBlock();
             }
+            while (block.Id == NextBlock.Id);
 
-            private Block RandomBlock()
-            {
-                return blocks[random.Next(blocks.Length)];
-            }
-
-            public Block GetAndUpdate()
-            {
-                Block block = NextBlock;
-
-                do
-                {
-                    NextBlock = RandomBlock();
-                }
-                while (block.Id == NextBlock.Id);
-
-                return block;
-            }
+            return block;
         }
     }
+}
+    
 
